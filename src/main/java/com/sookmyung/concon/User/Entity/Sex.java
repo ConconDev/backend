453c98fa3bd16858d1b78a98caf13952824defc7
osjkate <osjkate@sookmyung.ac.kr>
2024-08-11 22:58:06 +0900

    feat : [User] 엔티티 생성

---
+++ src/main/java/com/sookmyung/concon/User/Entity/Sex.java
@@ -0,0 +1,5 @@
+package com.sookmyung.concon.User.Entity;
+
+public enum Sex {
+    FEMALE, MALE
+}
---
+++ src/main/java/com/sookmyung/concon/User/Entity/User.java
@@ -0,0 +1,20 @@
+package com.sookmyung.concon.User.Entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class User {
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private Sex sex;
+    private int age;
+
+    // 프로필 사진
+    private String profileImage;
+}
