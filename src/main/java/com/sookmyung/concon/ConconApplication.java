676e21aa05c22d9a601203c3e774446ba776b738
osjkate <osjkate@sookmyung.ac.kr>
2024-08-11 22:49:32 +0900

    initial project setup

---
+++ src/main/java/com/sookmyung/concon/ConconApplication.java
@@ -0,0 +1,13 @@
+package com.sookmyung.concon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConconApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConconApplication.class, args);
+	}
+
+}
