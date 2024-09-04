aa43a5de8ba058b1b6e033f935d80969263067da
osjkate <osjkate@sookmyung.ac.kr>
2024-09-04 22:34:40 +0900

    feat : Authorization token 오류 해결

--- src/main/java/com/sookmyung/concon/configuration/SwaggerConfig.java
+++ src/main/java/com/sookmyung/concon/configuration/SwaggerConfig.java
@@ -2,14 +2,11 @@ package com.sookmyung.concon.configuration;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.Operation;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import org.springdoc.core.customizers.OperationCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.method.HandlerMethod;
 
 @Configuration
 public class SwaggerConfig {
