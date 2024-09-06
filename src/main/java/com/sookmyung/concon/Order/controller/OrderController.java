f700b074649b827d1d158404d9a9758834a6bdd5
osjkate <osjkate@sookmyung.ac.kr>
2024-09-06 19:09:00 +0900

    feat : OrderController, TransactionController 완료

--- src/main/java/com/sookmyung/concon/Order/controller/OrderController.java
+++ src/main/java/com/sookmyung/concon/Order/controller/OrderController.java
@@ -1,10 +1,9 @@
 package com.sookmyung.concon.Order.controller;
 
-import com.sookmyung.concon.Order.dto.OrderCreateRequestDto;
-import com.sookmyung.concon.Order.dto.OrderDetailResponseDto;
-import com.sookmyung.concon.Order.dto.OrderSimpleResponseDto;
+import com.sookmyung.concon.Order.dto.*;
 import com.sookmyung.concon.Order.service.OrderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,7 +20,14 @@ public class OrderController {
     public ResponseEntity<OrderDetailResponseDto> createOrders(
             @RequestHeader("Authorization") String token,
             @RequestBody OrderCreateRequestDto request) {
-        return ResponseEntity.ok(orderService.createOrder(request));
+        return ResponseEntity.status(HttpStatus.CREATED).body(orderService.createOrder(request));
+    }
+
+    // 거래 아이디로 단일 조회
+    @GetMapping("/{order-id}")
+    public ResponseEntity<OrderDetailResponseDto> getOrderByOrderId(
+            @PathVariable("order-id") Long orderId) {
+        return ResponseEntity.ok(orderService.getOrderByOrderId(orderId));
     }
 
     // 나의 판매 상품 전체 조회
@@ -52,17 +58,25 @@ public class OrderController {
     }
 
     // 아이템 종류로 조회 (판매중)
-    @GetMapping("/item/{item_id}")
+    @GetMapping("/item/{item-id}")
     public ResponseEntity<List<OrderSimpleResponseDto>> getAllOrdersByItemId(
-            @PathVariable("item_id") Long itemId,
+            @PathVariable("item-id") Long itemId,
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "5") int size) {
         return ResponseEntity.ok(orderService.getAllOrdersByItemId(itemId, page, size));
     }
 
-    // 거래 요청 & 수락
-
     // 거래 수정
+    @PutMapping
+    public ResponseEntity<OrderDetailResponseDto> modifyOrder(
+            @RequestBody OrderModifyRequestDto request) {
+        return ResponseEntity.ok(orderService.modifyOrder(request));
+    }
 
     // 거래 삭제
+    @DeleteMapping("{order-id}")
+    public ResponseEntity<Object> deleteOrder(
+            @PathVariable("order-id") Long orderId) {
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/com/sookmyung/concon/Order/service/OrderService.java
+++ src/main/java/com/sookmyung/concon/Order/service/OrderService.java
@@ -1,6 +1,5 @@
 package com.sookmyung.concon.Order.service;
 
-import com.sookmyung.concon.Alarm.eventPublisher.EventPublisher;
 import com.sookmyung.concon.Coupon.Entity.Coupon;
 import com.sookmyung.concon.Coupon.dto.CouponSimpleResponseDto;
 import com.sookmyung.concon.Coupon.repository.CouponRepository;
@@ -9,8 +8,6 @@ import com.sookmyung.concon.Item.repository.ItemRepository;
 import com.sookmyung.concon.Order.dto.*;
 import com.sookmyung.concon.Order.entity.OrderStatus;
 import com.sookmyung.concon.Order.entity.Orders;
-import com.sookmyung.concon.Order.exception.OrderInProgressException;
-import com.sookmyung.concon.Order.repository.OrderRequestRedisRepository;
 import com.sookmyung.concon.Order.repository.OrderRepository;
 import com.sookmyung.concon.User.Entity.User;
 import com.sookmyung.concon.User.Jwt.JwtUtil;
@@ -23,7 +20,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDate;
 import java.util.List;
 
 @Service
@@ -35,15 +31,6 @@ public class OrderService {
     private final UserRepository userRepository;
     private final ItemRepository itemRepository;
     private final JwtUtil jwtUtil;
-
-    private final OrderRequestRedisRepository orderRequestRedisRepository;
-    private final EventPublisher eventPublisher;
-
-
-    private static final String ORDER_REQUESTED = "orderRequested";
-    private static final String ORDER_ACCEPTED = "orderAccepted";
-    private static final String ORDER_CANCELED = "orderCanceled";
-    private static final String ORDER_COMPLETED = "orderCompleted";
 
     // 메소드 추출
     // 쿠폰 아이디로 찾기
@@ -70,6 +57,10 @@ public class OrderService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 주문을 조회할 수 없습니다."));
     }
 
+    /*
+    dto 변환 메소드 추출
+     */
+    // OrderDetailResponseDto 변환 메소드
     private static OrderDetailResponseDto toOrderDetailDto(Orders order) {
         Coupon coupon = order.getCoupon();
         return OrderDetailResponseDto.toDto(order,
@@ -77,6 +68,7 @@ public class OrderService {
                 , UserSimpleResponseDto.toDto(order.getBuyer()), UserSimpleResponseDto.toDto(order.getSeller()));
     }
 
+    // List<OrderSimpleResponseDto> 변환 메소드
     private static List<OrderSimpleResponseDto> toOrderSimpleDtoList(List<Orders> orders) {
         return orders.stream().map(order ->
                 OrderSimpleResponseDto.toDto(order,
@@ -99,7 +91,7 @@ public class OrderService {
                 null, UserSimpleResponseDto.toDto(seller));
     }
 
-    // 상품 아이디로 단일 조회
+    // 거래 아이디로 단일 조회
     @Transactional(readOnly = true)
     public OrderDetailResponseDto getOrderByOrderId(Long orderId) {
         Orders order = findOrdersById(orderId);
@@ -143,85 +135,6 @@ public class OrderService {
         Pageable pageable = PageRequest.of(page, size);
         List<Orders> orders = orderRepository.findAllByItemAndStatus(item, OrderStatus.AVAILABLE, pageable);
         return toOrderSimpleDtoList(orders);
-    }
-
-    // 거래 요청
-    @Transactional
-    public void requestOrder(Long orderId, String token) {
-        Orders orders = findOrdersById(orderId);
-        User buyer = findUserByToken(token);
-        if (orders.getStatus() == OrderStatus.IN_PROGRESS) {
-            throw new OrderInProgressException("이 주문은 이미 거래 중입니다. ");
-        }
-        orderRequestRedisRepository.save(orders.getId(), buyer.getId());
-
-        // 판매자에게 알람
-        Long sellerId = orders.getSeller().getId();
-        OrderEventAlarmDto response = OrderEventAlarmDto.toDto(orders);
-        eventPublisher.publishEvent(sellerId, ORDER_REQUESTED, response);
-    }
-
-    // 거래 요청 전체 조회
-    @Transactional(readOnly = true)
-    public List<UserSimpleResponseDto> getAllRequestOrder(Long orderId) {
-        return orderRequestRedisRepository.findById(orderId)
-                .stream().map(Long::parseLong)
-                .map(this::findUserById)
-                .map(UserSimpleResponseDto::toDto)
-                .toList();
-    }
-
-    // 거래 수락(거래 중)
-    @Transactional
-    public OrderDetailResponseDto acceptTransaction(TransactionAcceptRequestDto request) {
-        Orders order = findOrdersById(request.getOrderId());
-        User buyer = findUserById(request.getBuyerId());
-        order.setBuyer(buyer);
-        order.updateStatus(OrderStatus.IN_PROGRESS);
-
-        orderRequestRedisRepository.deleteUser(request.getOrderId(), request.getBuyerId());
-
-        // 구매자에게 알람
-        OrderEventAlarmDto response = OrderEventAlarmDto.toDto(order);
-        eventPublisher.publishEvent(buyer.getId(), ORDER_ACCEPTED, response);
-
-        return toOrderDetailDto(order);
-    }
-
-    // 거래 중 취소
-    @Transactional
-    public OrderDetailResponseDto cancelTransaction(Long orderId) {
-        Orders order = findOrdersById(orderId);
-        order.setBuyer(null);
-        order.updateStatus(OrderStatus.AVAILABLE);
-
-        // 둘 다에게 알람
-        List<Long> userIds = List.of(order.getSeller().getId(), order.getBuyer().getId());
-        OrderEventAlarmDto response = OrderEventAlarmDto.toDto(order);
-        eventPublisher.publishEventToMultipleUsers(userIds, ORDER_CANCELED, response);
-
-        return toOrderDetailDto(order);
-    }
-
-    // 거래 완료
-    @Transactional
-    public OrderDetailResponseDto completeTransaction(Long orderId) {
-        Orders order = findOrdersById(orderId);
-        order.setBuyer(null);
-        order.updateStatus(OrderStatus.COMPLETED);
-        order.setTransactionDate(LocalDate.now());
-
-        Coupon coupon = order.getCoupon();
-        coupon.changeUser(order.getBuyer());
-        coupon.setBuyFlag(true);
-
-        orderRequestRedisRepository.delete(orderId);
-
-        // 구매자에게 알람
-        OrderEventAlarmDto response = OrderEventAlarmDto.toDto(order);
-        eventPublisher.publishEvent(order.getBuyer().getId(), ORDER_COMPLETED, response);
-
-        return toOrderDetailDto(order);
     }
 
     // 거래 수정
