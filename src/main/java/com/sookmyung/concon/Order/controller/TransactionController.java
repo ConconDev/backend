10600d5e7c3dc4ff43bfb164d974ea74d8e43f6c
osjkate <osjkate@sookmyung.ac.kr>
2024-09-17 23:43:49 +0900

    docs: api endpoint에 대한 swagger docs 추가

--- src/main/java/com/sookmyung/concon/Order/controller/TransactionController.java
+++ src/main/java/com/sookmyung/concon/Order/controller/TransactionController.java
@@ -5,12 +5,15 @@ import com.sookmyung.concon.Order.dto.OrderRequestResponseDto;
 import com.sookmyung.concon.Order.dto.TransactionAcceptRequestDto;
 import com.sookmyung.concon.Order.service.TransactionService;
 import com.sookmyung.concon.User.dto.UserSimpleResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "거래 API")
 @RestController
 @RequestMapping("/api/transaction")
 @RequiredArgsConstructor
@@ -18,6 +21,7 @@ public class TransactionController {
     private final TransactionService transactionService;
 
     // 거래 요청
+    @Operation(summary = "거래 요청", description = "거래를 원하는 상품에게 거래 요청 -> 거래 요청 대기 목록으로 들어감")
     @PostMapping("/request/{order-id}")
     public ResponseEntity<OrderRequestResponseDto> requestOrder(
             @PathVariable("order-id") Long orderId,
@@ -26,6 +30,7 @@ public class TransactionController {
     }
 
     // 거래 요청 전체 조회
+    @Operation(summary = "거래 요청 전체 조회", description = "상품을 클릭하면 해당 상품 거래 요청 대기목록을 확인할 수 있음")
     @GetMapping("/request/{order-id}")
     public ResponseEntity<List<UserSimpleResponseDto>> getAllRequestOrder(
             @PathVariable("order-id") Long orderId) {
@@ -33,6 +38,7 @@ public class TransactionController {
     }
 
     // 거래 수락 (거래 중)
+    @Operation(summary = "거래 수락 -> 거래 중", description = "대기 목록 중 하나를 누르면 거래를 수락하고 거래 중으로 바뀜, 거래 중이면 다른 사람은 거래 요청을 할 수 없음")
     @PostMapping("/accept")
     public ResponseEntity<OrderDetailResponseDto> acceptTransaction(
             @RequestBody TransactionAcceptRequestDto request) {
@@ -40,6 +46,7 @@ public class TransactionController {
     }
 
     // 거래 중 취소
+    @Operation(summary = "거래 중 취소", description = "거래 중, 입금을 안하거나 다른 사유가 있어서 거래 취소를 할 수 있음")
     @PostMapping("/cancel/{order-id}")
     public ResponseEntity<OrderDetailResponseDto> cancelTransaction(
             @PathVariable("order-id") Long orderId) {
@@ -47,6 +54,7 @@ public class TransactionController {
     }
 
     // 거래 완료
+    @Operation(summary = "거래 완료", description = "거래 중, 입금을 확인하고 거래가 완료되면 거래 상태를 완료로 전환")
     @PostMapping("/complete/{order-id}")
     public ResponseEntity<OrderDetailResponseDto> completeTransaction(
             @PathVariable("order-id") Long orderId) {
