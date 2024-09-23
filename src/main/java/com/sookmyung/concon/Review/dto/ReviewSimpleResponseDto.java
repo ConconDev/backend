56e9c93bcf099c536e81a6b46db41c02b771b207
osjkate <osjkate@sookmyung.ac.kr>
2024-09-24 00:12:25 +0900

    feat: 리뷰 simple 조회 수정

--- src/main/java/com/sookmyung/concon/Review/dto/ReviewSimpleResponseDto.java
+++ src/main/java/com/sookmyung/concon/Review/dto/ReviewSimpleResponseDto.java
@@ -8,6 +8,7 @@ import lombok.Getter;
 @Builder
 public class ReviewSimpleResponseDto {
     private Long reviewId;
+    private Long userId;
     private Long itemId;
     private double score;
     private String content;
@@ -15,6 +16,7 @@ public class ReviewSimpleResponseDto {
     public static ReviewSimpleResponseDto toDto(Review review) {
         return ReviewSimpleResponseDto.builder()
                 .reviewId(review.getId())
+                .userId(review.getUser().getId())
                 .itemId(review.getCoupon().getItem().getId())
                 .score(review.getScore())
                 .content(review.getContent())
