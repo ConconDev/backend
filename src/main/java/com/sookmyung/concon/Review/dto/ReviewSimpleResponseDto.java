86154b96c7db35e858138400a6de6842304791d2
osjkate <osjkate@sookmyung.ac.kr>
2024-09-24 00:08:17 +0900

    feat: 리뷰 simple 조회에 아이템 아이디 추가

--- src/main/java/com/sookmyung/concon/Review/dto/ReviewSimpleResponseDto.java
+++ src/main/java/com/sookmyung/concon/Review/dto/ReviewSimpleResponseDto.java
@@ -8,12 +8,14 @@ import lombok.Getter;
 @Builder
 public class ReviewSimpleResponseDto {
     private Long reviewId;
+    private Long itemId;
     private double score;
     private String content;
 
     public static ReviewSimpleResponseDto toDto(Review review) {
         return ReviewSimpleResponseDto.builder()
                 .reviewId(review.getId())
+                .itemId(review.getCoupon().getItem().getId())
                 .score(review.getScore())
                 .content(review.getContent())
                 .build();
